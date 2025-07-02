efc82f231ebab814b09a97a3b46891660efc790e
samiksha patre <sampatreo4@gmail.com>
2025-07-02 17:22:02 +0530

    React App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Welcome to My React UI 🌟</h1>
+        <p>This is a clean and creative interface built with React.</p>
+        <button className="glow-button">
+          Click Me
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
